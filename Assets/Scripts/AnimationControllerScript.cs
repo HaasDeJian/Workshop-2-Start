d7278a785f4f1879d809480a8a0a7ef1f99ddbf4
Jian Haas, de <dehaasjian@gmail.com>
2024-03-18 11:21:59 +0100

    Added health text begin function

--- Assets/Scripts/AnimationControllerScript.cs
+++ Assets/Scripts/AnimationControllerScript.cs
@@ -1,18 +1,50 @@
 using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class AnimationControllerScript : MonoBehaviour
 {
+    // Public fields
+    [SerializeField] private float acceleration = 0.5f, deceleration = 0.5f, maximumInjuredLayerWeight = 0.5f;
+    [SerializeField] private TextMeshProUGUI healthText;
+
+    
+    // Player health
+    private const float MaximumHealth = 100;
+    private float _currentHealth;
+    
     // Start is called before the first frame update
     private void Start()
     {
-       
+        _currentHealth = MaximumHealth;
     }
 
     // Update is called once per frame
     private void Update()
+    { 
+        ChangeLayers();
+    }
+    
+    private void ChangeLayers()
     {
-        
+        var hitPressed = Input.GetKeyDown(KeyCode.Space);
+
+        if (hitPressed)
+        {
+            _currentHealth -= MaximumHealth / 10;
+
+            if (_currentHealth < 0)
+            {
+                _currentHealth = MaximumHealth;
+            }
+        }
+
+        var healthPercentage = _currentHealth / MaximumHealth;
+        healthText.text = $"Health: {healthPercentage * 100}%";
+
+        // Change (and add) code underneath
+        var currentInjuredLayerWeight = 0;
+        var targetInjuredLayerWeight = (1 - healthPercentage) * maximumInjuredLayerWeight;
     }
 }
